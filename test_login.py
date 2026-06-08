ee5f86ab2def9354373dd6dc5ceb002897011994
huy.nguyen <Huy21012000@>
2026-06-08 15:04:39 +0700

    Implement recruitment link

--- test_login.py
+++ test_login.py
@@ -8,13 +8,17 @@ def test_login(driver):
     password_field = (By.NAME, 'password')
     click_btn = (By.XPATH, '//button[@type="submit"]')
     upgrade_btn = (By.XPATH, '//button[@class="oxd-glass-button orangehrm-upgrade-button"]')
+    recruitment_btn = (By.XPATH, '//a[contains(@href,"recruitment")]')
+
 
     driver.find_element(*username_field).send_keys('Admin')
 
     driver.find_element(*password_field).send_keys('admin123')
     driver.find_element(*click_btn).click()
+    driver.find_element(*recruitment_btn).click()
 
     sleep(7)
     #upgrade_btn = WebDriverWait(driver, 10).until(lambda d: d.find_element(*upgrade_btn))
+
 
     assert driver.find_element(*upgrade_btn).is_displayed()
