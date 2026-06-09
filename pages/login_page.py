9c923f7c30bfc8cba46ce0910e769d9d3033944e
unknown <huynhcongduy2002@gmail.com>
2026-06-09 20:56:33 +0700

    improve selenium code

---
+++ pages/login_page.py
@@ -0,0 +1,22 @@
+import pytest
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class LoginPage:
+    def __init__(self, driver):
+        self.driver = driver
+
+        self.username_field = (By.NAME, 'username')
+        self.password_field = (By.NAME, 'password')
+        self.click_btn = (By.XPATH, '//button[@type="submit"]')
+        self.upgrade_btn = (By.XPATH, '//button[@class="oxd-glass-button orangehrm-upgrade-button"]')
+    def login(self, username, password):
+        self.driver.find_element(*self.username_field).send_keys('Admin')
+        self.driver.find_element(*self.password_field).send_keys('admin123')
+        self.driver.find_element(*self.click_btn).click()
+
+    def is_upgrade_button_displayed(self):
+        return WebDriverWait(self.driver, 10).until(lambda d: d.find_element(*self.upgrade_btn)).is_displayed()
