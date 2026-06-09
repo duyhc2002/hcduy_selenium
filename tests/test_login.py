5b8fe54a9bd6b4da2abbc18723fb68cf0c5285c6
unknown <huynhcongduy2002@gmail.com>
2026-06-09 20:56:33 +0700

    improve selenium code

---
+++ tests/test_login.py
@@ -0,0 +1,11 @@
+import pytest
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from pages.login_page import LoginPage
+
+def test_login(driver):
+    login_page = LoginPage(driver)
+    login_page.login("Admin", "admin123")
+    login_page.is_upgrade_button_displayed()
