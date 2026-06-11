2660017d3452391ed9b8db3719d983e2a9a4df39
unknown <huynhcongduy2002@gmail.com>
2026-06-11 20:54:47 +0700

    Update recruitment page code

--- pages/recruitment_page.py
+++ pages/recruitment_page.py
@@ -8,15 +8,13 @@ class RecruitmentPage:
     def __init__(self, driver):
         self.driver = driver
 
-        self.username_field = (By.NAME, 'username')
-        self.password_field = (By.NAME, 'password')
-        self.click_btn = (By.XPATH, '//button[@type="submit"]')
         self.recruitment_btn = (By.XPATH, '//a[contains(@href,"recruitment")]')
+        self.vacancies_btn = (By.LINK_TEXT, "Vacancies")
+        self.job_title = (By.XPATH, "//div[@role='option' and normalize-space()='Database Administrator']")
         self.upgrade_btn = (By.XPATH, '//button[@class="oxd-glass-button orangehrm-upgrade-button"]')
-    def login(self, username, password):
-        self.driver.find_element(*self.username_field).send_keys('Admin')
-        self.driver.find_element(*self.password_field).send_keys('admin123')
-        self.driver.find_element(*self.click_btn).click()
+    def recruitment(self):
         self.driver.find_element(*self.recruitment_btn).click()
+        self.driver.find_element(*self.vacancies_btn).click()
+        self.driver.find_element(*self.job_title).click()
     def is_upgraded_button_displayed(self):
         return WebDriverWait(self.driver, 10).until(lambda d: d.find_element(*self.upgrade_btn)).is_displayed()
--- tests/test_recruitment.py
+++ tests/test_recruitment.py
@@ -3,10 +3,14 @@ from selenium import webdriver
 from time import sleep
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+from pages.login_page import LoginPage
 from pages.recruitment_page import RecruitmentPage
 
 def test_recruitment(driver):
+    login_page = LoginPage(driver)
+    login_page.login("Admin", "admin123")
+    sleep(3)
     recruitment_page = RecruitmentPage(driver)
-    recruitment_page.login("Admin", "admin123")
+    recruitment_page.recruitment()
     sleep(3)
     recruitment_page.is_upgraded_button_displayed()
